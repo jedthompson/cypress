f87934f5941a6fbb4a8d74f13c65003d1f0cdde9
Jed Thompson <thompson.jed@gmail.com>
2011-06-24 11:02:08 -0400

    Started adding new simulations code (block on inclined plane)

---
+++ src/js/common/simulation.js
@@ -0,0 +1,75 @@
+function Simulation(name) {
+	this.name = name;
+	this.description = "Simulation: "+name;
+	this.canvas = null;
+	this.context = null;
+
+	// the space in milliseconds between invocations of 'step'
+	this.dt = 20;
+
+	this.state = {
+		settings: {},
+	}
+
+	this.step = function(state) {
+		return state;
+	}
+	this.render2d = null;
+	this.render3d = null;
+
+	this.renderSimulation = function (s, c, w, h) {
+		if (this.render2d != null) {
+			this.render2d(s, c, w, h);
+		}
+	}
+	this.renderDescription = function (s, c, w, h) {
+		// just clear the canvas to expose the description
+		c.clearRect(-w/2, -h/2, w/2, h/2);
+	}
+	this.renderSettings = function(s, c, w, h) {
+
+	}
+
+	this.tabs = {
+		Description: this.renderDescription.bind(this),
+		Simulation: this.renderSimulation.bind(this),
+		Settings: this.renderSettings.bind(this),
+	}
+
+	this.currentTab = "Simulation";
+
+	this.addTab = function(name, render) {
+		this.tabs[name] = render.bind(this);
+	}
+
+	this.start = function() {
+		if (this.canvas == null) {
+			alert("Error!");
+		}
+		canvas = this.canvas;
+		this.context = canvas.getContext("2d");
+		this.run();
+	}
+
+	// recursive function to run step() and render()
+	this.run = function () {
+		setTimeout(this.run.bind(this), this.dt);
+		this.state = this.step(this.state);
+		this.context.clearRect(0, 0, this.width, this.height);
+		this.context.fillStyle='white';
+		this.context.fillRect(0, 0, this.width, this.height);
+		this.context.fillStyle='black';
+
+		c = this.context;
+		w = this.width;
+		h = this.height;
+		c.save();
+		this.context.lineWidth=0.4;
+		c.translate(w/2, h/2);
+		sf = (w>h)?(h/100):(w/100);
+		c.scale(sf,sf);
+		this.tabs[this.currentTab](this.state, c, w/sf, h/sf);
+		c.restore();
+	}
+}
+
---
+++ src/js/simulations/nuclear/misc/decay.js
@@ -0,0 +1,76 @@
+var simulation_name = "Nuclear Decay";
+
+var simulation = new Simulation(simulation_name);
+simulation.dt = 50;
+
+function init_state(state) {
+	state.history = {};
+	state.pc = 100;
+	state.hl = 30;
+	state.dc = Math.LN2/state.hl;
+	state.t = 0;
+	state.pd = state.dc;
+	state.history[state.t] = state.pc;
+
+	state.decayed = new Array(10);
+	for (var x=0; x<10; x++) {
+		state.decayed[x] = new Array(10);
+		for (var y=0; y<10; y++) {
+			state.decayed[x][y] = false;
+		}
+	}
+	return state;
+}
+simulation.state = init_state(simulation.state);
+
+simulation.step = function(state) {
+	state.t++;
+	state.pc = state.pc*(1-state.pd);
+
+	for (var x=0; x<10; x++) {
+		for (var y=0; y<10; y++) {
+			if (Math.random() < state.pd) {
+				state.decayed[x][y] = true;
+			}
+		}
+	}
+	state.history[state.t] = state.pc;
+
+	if (state.t > 400) { // TODO put a reset button somewhere
+		state = init_state(state);
+	}
+	return state;
+}
+
+simulation.render2d = function(state, c, w, h) {
+	for (var x=0; x<10; x++) {
+		for (var y=0; y<10; y++) {
+			if (!state.decayed[x][y]) {
+				c.drawImage(window.images["particle-red"],
+					(8*x)-40, (8*y)-40, 8, 8);
+			} else {
+				c.drawImage(window.images["particle-grey"],
+					(8*x)-40, (8*y)-40, 8, 8);
+			}
+		}
+	}
+}
+
+simulation.renderGraph = function(state, c, w, h) {
+	c.beginPath();
+	c.moveTo(-40, -40);
+	c.lineTo(-40, 40);
+	c.lineTo(40, 40);
+	c.stroke();
+
+	c.beginPath();
+	c.moveTo(-40, 0);
+	for (var t=0; t<=Math.min(state.t,400); t++) {
+		c.lineTo(-40+t/5, 40-state.history[t]*0.6);
+	}
+	//c.lineTo(20+t, 280-pc*2);
+	c.stroke();
+}
+
+simulation.addTab('Graph', simulation.renderGraph);
+
