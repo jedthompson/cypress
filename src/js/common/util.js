5e0dad4d02b6d5b698225b23e7dea463f801c949
Scott Lawrence <bytbox@gmail.com>
2011-06-25 16:20:44 -0400

    Header comment added to makefile

---
+++ src/js/common/util.js
@@ -0,0 +1,8 @@
+function curry(fn) {
+        var slice = Array.prototype.slice;
+        var args = slice.apply(arguments, [1]);
+        return function () {
+                return fn.apply(null, args.concat(slice.apply(arguments)));
+        };
+}
+
