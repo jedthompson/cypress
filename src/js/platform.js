d41985b9350260f1e36de9a6f59ae19c1f8085ac
Scott Lawrence <bytbox@gmail.com>
2011-06-26 15:20:52 -0400

    (modified) conditional loading framework now in use

--- src/js/platform.js
+++ src/js/platform.js
@@ -1,46 +1,21 @@
-window.images = {
-	"particle-red": getImage("all/particle-red.png"),
-	"particle-blue": getImage("all/particle-blue.png"),
-	"particle-grey": getImage("all/particle-grey.png"),
-	"IOSSliderWhite": getImage("iPhone/GenericSlider/SliderWhite.png"),
-	"IOSSliderBlue": getImage("iPhone/GenericSlider/SliderBlue.png"),
-	"IOSSliderCenter": getImage("iPhone/GenericSlider/SliderCenter.png"),
-}
-
-window.resources = {}
-
-window.resources.gr = {
-	"all": {
-		"particle-red": image("all/particle-red.png"),
-		"particle-blue": image("all/particle-blue.png"),
-		"particle-grey": image("all/particle-grey.png"),
-	},
-	"iOS": {
-		"IOSSliderWhite": image("iPhone/GenericSlider/SliderWhite.png"),
-		"IOSSliderBlue": image("iPhone/GenericSlider/SliderBlue.png"),
-		"IOSSliderCenter": image("iPhone/GenericSlider/SliderCenter.png"),
-	},
-	"android": {
-	},
-}
-
-window.resources.css = {
-	"all": {},
-	"iOS": {},
-	"android": {},
-}
-
-window.resources.js = {
-	"all": {},
-	"iOS": {},
-	"android": {},
+window.images = {}
+window.resources = {
+	"all": [
+		image("particle-red", "all/particle-red.png"),
+		image("particle-blue", "all/particle-blue.png"),
+		image("particle-grey", "all/particle-grey.png"),
+	],
+	"iOS": [
+		image("IOSSliderWhite", "iPhone/GenericSlider/SliderWhite.png"),
+		image("IOSSliderBlue", "iPhone/GenericSlider/SliderBlue.png"),
+		image("IOSSliderCenter", "iPhone/GenericSlider/SliderCenter.png"),
+	],
+	"android": [
+	],
 }
 
 function getPlatformResources(platform) {
-	var res = {};
-	res.gr = Object.extend(window.resources.gr.all, window.resources.gr[platform]);
-	res.css = Object.extend(window.resources.css.all, window.resources.css[platform]);
-	res.js = Object.extend(window.resources.js.all, window.resources.js[platform]);
+	var res = window.resources.all.concat(window.resources[platform]);
 	return res;
 }
 
@@ -50,9 +25,33 @@ function getResources() {
 	return getPlatformResources('all');
 }
 
-function image(name) {
+function loadResources() {
+	rset = getResources();
+	rset.forEach(function (f) {f()});
+}
+
+function image(name, filename) {
 	return function() {
-		return getImage(name);
+		window.images[name] = getImage(filename);
 	}
 }
+
+function stylesheet(filename) {
+	return function() {
+		var stylesheet = document.createElement('link');
+		stylesheet.setAttribute('rel', 'stylesheet');
+		stylesheet.setAttribute('href', '../css/'+filename);
+		document.getElementsByTagName('head')[0].appendChild(stylesheet);
+	}
+}
+
+function script(name) {
+	return function() {
+		var script = document.createElement('script');
+		script.setAttribute('src', '../js/'+filename);
+		document.getElementsByTagName('head')[0].appendChild(script);
+	}
+}
+
+loadResources();
 
