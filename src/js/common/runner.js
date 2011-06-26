7d96943fa33ff6c3b34fa73afad6576d05f06752
Scott Lawrence <bytbox@gmail.com>
2011-06-26 14:26:31 -0400

    Moving image imports to platform.js

--- src/js/common/runner.js
+++ src/js/common/runner.js
@@ -7,15 +7,6 @@ var the_sim;
 
 window.onresize = sizeCanvas;
 
-window.images = {
-	"particle-red": getImage("all/particle-red.png"),
-	"particle-blue": getImage("all/particle-blue.png"),
-	"particle-grey": getImage("all/particle-grey.png"),
-	"IOSSliderWhite": getImage("iPhone/GenericSlider/SliderWhite.png"),
-	"IOSSliderBlue": getImage("iPhone/GenericSlider/SliderBlue.png"),
-	"IOSSliderCenter": getImage("iPhone/GenericSlider/SliderCenter.png"),
-}
-
 function sizeCanvas() {
 	sim = the_sim;
 
---
+++ src/js/platform.js
@@ -0,0 +1,8 @@
+window.images = {
+	"particle-red": getImage("all/particle-red.png"),
+	"particle-blue": getImage("all/particle-blue.png"),
+	"particle-grey": getImage("all/particle-grey.png"),
+	"IOSSliderWhite": getImage("iPhone/GenericSlider/SliderWhite.png"),
+	"IOSSliderBlue": getImage("iPhone/GenericSlider/SliderBlue.png"),
+	"IOSSliderCenter": getImage("iPhone/GenericSlider/SliderCenter.png"),
+}
