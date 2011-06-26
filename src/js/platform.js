0aefcd70be743cedf993bde2506f911f4e60aeef
Scott Lawrence <bytbox@gmail.com>
2011-06-26 14:58:10 -0400

    Conditional loading framework in place and functional

--- src/js/platform.js
+++ src/js/platform.js
@@ -6,3 +6,53 @@ window.images = {
 	"IOSSliderBlue": getImage("iPhone/GenericSlider/SliderBlue.png"),
 	"IOSSliderCenter": getImage("iPhone/GenericSlider/SliderCenter.png"),
 }
+
+window.resources = {}
+
+window.resources.gr = {
+	"all": {
+		"particle-red": image("all/particle-red.png"),
+		"particle-blue": image("all/particle-blue.png"),
+		"particle-grey": image("all/particle-grey.png"),
+	},
+	"iOS": {
+		"IOSSliderWhite": image("iPhone/GenericSlider/SliderWhite.png"),
+		"IOSSliderBlue": image("iPhone/GenericSlider/SliderBlue.png"),
+		"IOSSliderCenter": image("iPhone/GenericSlider/SliderCenter.png"),
+	},
+	"android": {
+	},
+}
+
+window.resources.css = {
+	"all": {},
+	"iOS": {},
+	"android": {},
+}
+
+window.resources.js = {
+	"all": {},
+	"iOS": {},
+	"android": {},
+}
+
+function getPlatformResources(platform) {
+	var res = {};
+	res.gr = Object.extend(window.resources.gr.all, window.resources.gr[platform]);
+	res.css = Object.extend(window.resources.css.all, window.resources.css[platform]);
+	res.js = Object.extend(window.resources.js.all, window.resources.js[platform]);
+	return res;
+}
+
+function getResources() {
+	if (navigator.userAgent.match(/like Mac OS X/i))
+		return getPlatformResources('iOS');
+	return getPlatformResources('all');
+}
+
+function image(name) {
+	return function() {
+		return getImage(name);
+	}
+}
+
