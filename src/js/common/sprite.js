e14dc8784c5957ca29623a0eeac067a0781cab6c
Scott Lawrence <bytbox@gmail.com>
2011-06-28 14:55:28 -0400

    SpriteSet stubs

--- src/js/common/sprite.js
+++ src/js/common/sprite.js
@@ -1,3 +1,12 @@
 // Sprite manipulation
 
+function SpriteSet2D(limit, res) {
+	this.gridSize = vBO(function(a,b){return a/b})(limit, res);
+	this.data = {}
+}
+
+function SpritSet3D(limit, res) {
+	this.gridSize = vBO(function(a,b){return a/b})(limit, res);
+	this.data = {}
+}
 
