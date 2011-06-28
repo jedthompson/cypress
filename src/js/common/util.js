7a2114bab11b4103bf24e29141de053aa815aeb0
Scott Lawrence <bytbox@gmail.com>
2011-06-28 12:11:05 -0400

    util.js: adding binarySearch()

--- src/js/common/util.js
+++ src/js/common/util.js
@@ -6,3 +6,20 @@ function curry(fn) {
         };
 }
 
+// Performs binary search, on the assumption that p(v) is false on the left and
+// true on the right.
+function binarySearch(p, d, s, e) {
+	v = (s+e)/2;
+	dist = (e-s)/4;
+	for (i=0; i<d; i++) {
+		if (p(v)) // move to the left
+			v -= dist;
+		else // move to the right
+			v += dist;
+		dist /= 2;
+	}
+	return v;
+}
+
+alert(binarySearch(function (x) {return x>2}, 10, 0, 4));
+
