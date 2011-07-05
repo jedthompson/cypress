a148be1a8acf7de5afdb0c3cd611d801aa9fb0d2
Scott Lawrence <bytbox@gmail.com>
2011-07-05 11:36:28 -0400

    The beginnings of a widget system

--- src/js/common/widgets.js
+++ src/js/common/widgets.js
@@ -1,3 +1,10 @@
+function Widget(width, height, render, listener) {
+	this.width = width;
+	this.height = height;
+	this.render = render;
+	this.listener = listener;
+}
+
 //PercentFull is given AS A DECIMAL (.5 = 50%)
 function renderIOSSlider(xPosLeftOfBar, yPosTopOfBar, length, percentFull, context) {
 	var xTL = xPosLeftOfBar;
@@ -12,14 +19,8 @@ function renderIOSSlider(xPosLeftOfBar, yPosTopOfBar, length, percentFull, conte
 	var pXTL = xTL*480/100;
 	var pYTL = yTL*320/100;
 	var pCurLen = curPos*pLen;
-	
-	
-	
-	
-	
-	
+
 	ctx.image(window.images["IOSSliderBlue"], 0, 0, pCurLen, 9, xTL, yTL, curPos*len, 9/4.8);
 	ctx.image(window.images["IOSSliderWhite"], (480-pLen+pCurLen), 0, pLen-pCurLen, 9, xTL+(curPos*len), yTL, (len-curPos*len), 9/4.8);
 	ctx.image(window.images["IOSSliderCenter"], xTL+(curPos*len)-12/4.8, yTL-(7/4.8), 23/4.8, 23/4.8);
-	
 }
