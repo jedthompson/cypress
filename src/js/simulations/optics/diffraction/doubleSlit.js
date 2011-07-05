00bc5a9587baec9b0e11bd6d610ea444a811d0da
Scott Lawrence <bytbox@gmail.com>
2011-07-05 09:47:56 -0400

    Making waves more frequent and proportionally reducing the space in between slits

--- src/js/simulations/optics/diffraction/doubleSlit.js
+++ src/js/simulations/optics/diffraction/doubleSlit.js
@@ -7,9 +7,9 @@ simulation.description = "TODO Description";
 function init_state(state) {
 	state.xPos = [];
 	state.t = 0;
-	state.distBetWaves = 10; //How often a new wave is created, in units, equivalent to wavelength
+	state.distBetWaves = 5; //How often a new wave is created, in units, equivalent to wavelength
 	state.vel = 10; //Rate at which a wave propagates
-	state.distBetSlits = 20;
+	state.distBetSlits = 10;
 	
 	
 	state.xPos.push(-50);
@@ -95,59 +95,16 @@ simulation.render2d = function(state, c, w, h) {
 		c.stroke();
 	}
 	
-	//UNCOMMENT this stuff for COLOR MODE (a bit laggy though)
-	/*c.lineWidth=.6;
-	for(var i = -70; i <= 100; i+=.5) {
-		if(i < 0) {
-			var dist = (Math.abs(i-state.xPos[0]))%state.distBetWaves;
-			var phi = dist*2*Math.PI/state.distBetWaves;
-			var cosine = Math.cos(phi);
-			if(cosine > 0) {
-				c.beginPath();
-				c.moveTo(i, -h/2);
-				c.lineTo(i, h/2);
-				var red = Math.floor(cosine*255);
-				c.strokeStyle = 'rgb(' + red + ',0,0)';
-				c.stroke();
-			}else {
-				c.beginPath();
-				c.moveTo(i, -h/2);
-				c.lineTo(i, h/2);
-				var blue = Math.floor(Math.abs(cosine)*255);
-				c.strokeStyle = 'rgb(0,0,' + blue + ')';
-				c.stroke();
-			}
-		} else {
-			var dist = (Math.abs(i-state.xPos[0]))%state.distBetWaves;
-			var phi = dist*2*Math.PI/state.distBetWaves;
-			var cosine = Math.cos(phi);
-			if(cosine > 0) {
-				c.beginPath();
-				c.arc(0, 0, i, -Math.PI/2, Math.PI/2, false);
-				var red = Math.floor(cosine*255);
-				c.strokeStyle = 'rgb(' + red + ',0,0)';
-				c.stroke();
-			}else {
-			c.beginPath();
-				c.arc(0, 0, i, -Math.PI/2, Math.PI/2, false);
-				var blue = Math.floor(Math.abs(cosine)*255);
-				c.strokeStyle = 'rgb(0,0,' + blue + ')';
-				c.stroke();
-			}
-		}
-	}
-	c.lineWidth = .4;
-	c.strokeStyle="0f0"/**/
-	
+
 	c.fillStyle="#fff";
 	c.fillRect(-.3, -h/2, .6, h);
 	
 	c.beginPath();
 	c.moveTo(0, -h/2);
-	c.lineTo(0, -10.5);
-	c.moveTo(0, -9.5);
-	c.lineTo(0, 9.5);
-	c.moveTo(0, 10.5);
+	c.lineTo(0, -5.5);
+	c.moveTo(0, -4.5);
+	c.lineTo(0, 4.5);
+	c.moveTo(0, 5.5);
 	c.lineTo(0, h/2);
 	c.strokeStyle = "#000";
 	c.stroke();
