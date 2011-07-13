7668690369dfade747880667400a31cf77efead5
Scott Lawrence <bytbox@gmail.com>
2011-07-13 15:33:07 -0400

    Double-tab'd layout mostly works

---
+++ src/js/simulations/nuclear/misc/goldFoil.js
@@ -0,0 +1,87 @@
+var simulation_name = "Gold Foil Experiment";
+
+var simulation = new Simulation(simulation_name);
+simulation.dt = 20;
+simulation.description = "This simulation models the famous experiment performed by Rutherford that gave us our first idea of a nucleus.  Alpha particles (helium nuclei with a positive charge) were fired at a very thin piece of gold foil, with the expectation that they would easily penetrate the weak material, and indeed most of them did.  But a few alpha particles ricocheted back, having come near the extremely positive nucleus of the gold atom.  The fact that only a few bounced back indicates how small the nucleus really is compared to the rest of the atom.";
+    
+// Represents a field step
+var Charge = function(p, v) {
+	this.pos = p;
+	this.vel = v;
+}
+
+simulation.init_state = function(state) {
+	state.nucPos = new Vector(0, 0, 0);
+	state.nucVel = new Vector(0, 0, 0);
+	state.nucMass = 1;
+	state.nucCharge = 79;
+	state.alphaCharge = 2;
+	state.alphaMass = 1;
+	state.initAlphaVel = new Vector(25, 0, 0);
+	
+	state.nucRad = 2;
+	state.alphaRad = 1;
+	
+	state.history = [];
+	for(var i = 0; i < 360/10; i++) {
+		state.history[i] = 0;
+	}
+	
+	state.charges = [];
+	
+	state.t = 0;
+	return state;
+}
+simulation.state = simulation.init_state(simulation.state);
+
+function getForce(x, y, state) {
+	var dirVect = new Vector(x-state.nucPos.data[0], y-state.nucPos.data[1], 0);
+	force = dirVect.scale(state.nucCharge*state.alphaCharge/Math.pow(magV(dirVect),3));
+	return force;
+}
+
+simulation.step = function(state) {
+	state.t++;
+	if(state.t%2==0) {
+		var y = Math.random()*50-25;
+		state.charges.push(new Charge(new Vector(-simulation.getWidth()/2, y, 0), state.initAlphaVel));
+	}
+	for(var i = 0; i < state.charges.length; i++) {
+		var acc = getForce(state.charges[i].pos.data[0], state.charges[i].pos.data[1], state).scale(1/state.alphaMass);
+		state.charges[i].vel = addV(state.charges[i].vel, acc.scale(.001*simulation.dt));
+		state.charges[i].pos = addV(state.charges[i].pos, state.charges[i].vel.scale(.001*simulation.dt));
+		if(state.charges[i].pos.data[0] > simulation.getWidth() || state.charges[i].pos.data[0] < -simulation.getWidth() || state.charges[i].pos.data[1] > simulation.getHeight() || state.charges[i].pos.data[1] < -simulation.getHeight()) {
+			var theta = Math.atan2(state.charges[i].vel.data[1], state.charges[i].vel.data[0]);
+			state.history[Math.floor(theta*180/Math.PI/10+18)]++;
+			state.charges.splice(i, 1);
+		}
+	}
+	return state;
+}
+
+simulation.render2d = function(state, c, w, h) {
+	
+	c.beginPath();
+	c.arc(state.nucPos.data[0], state.nucPos.data[1], state.nucRad, 0, 2*Math.PI, false);
+	c.strokeStyle = "#000";
+	c.stroke();
+	
+	c.strokeStyle = "#f00";
+	for(var i = 0; i < state.charges.length; i++) {
+		c.beginPath();
+		c.arc(state.charges[i].pos.data[0], state.charges[i].pos.data[1], state.alphaRad, 0, 2*Math.PI, false);
+		c.stroke();
+	}
+}
+
+simulation.renderGraph = function(state, c, w, h) {
+	var arr = [];
+	for(var i = 0; i < 360/10; i++) {
+		var v = new Vector(i, state.history[i]);
+		arr.push(v);
+	}
+	drawGraph(new Vector(-40, -40), new Vector(40, 40), c, arr, true, "#000");
+}
+
+simulation.addTab("Graph", simulation.renderGraph);
+
---
+++ src/js/simulations/optics/diffraction/doubleSlit.js
@@ -0,0 +1,131 @@
+var simulation_name = "Double Slit Diffraction";
+
+var simulation = new Simulation(simulation_name);
+simulation.dt = 20;
+simulation.description = "As shown in the famous Young experiment, light behaves like a wave in that it diffracts.  This means that when passed through two slits, collated light will create an interference pattern.";
+
+simulation.init_state = function(state) {
+	state.xPos = [];
+	state.t = 0;
+	state.distBetWaves = 5; //How often a new wave is created, in units, equivalent to wavelength
+	state.vel = 10; //Rate at which a wave propagates
+	state.distBetSlits = 20;
+	
+	
+	state.xPos.push(-50);
+	
+	state.num = 0;
+	
+	state.colorTable = [];
+	state.xWaveTable = [];
+	
+	return state;
+}
+simulation.state = simulation.init_state(simulation.state);
+
+simulation.step = function(state) {
+	state.t++;
+	if(state.xPos[state.num] >= state.distBetWaves-50) {
+		state.xPos.push(-50);
+		state.num++;
+	}
+	for(var i = 0; i <= state.num; i++) {
+		state.xPos[i] += state.vel*.001*simulation.dt;
+		if(state.xPos[0] >= 70) {
+			state.xPos = state.xPos.slice(1);
+			state.num--;
+		}
+	}
+	
+	
+	if(state.t == 1) {
+		var h = simulation.getHeight();
+		var w = simulation.getWidth();
+	
+		for(var i = -h/2; i <= h/2; i += .3) {
+			var dist1 = Math.sqrt(Math.pow(i-state.distBetSlits/2, 2) + 1600);
+			var dist2 = Math.sqrt(Math.pow(i+state.distBetSlits/2, 2) + 1600);
+			var diff = Math.abs(dist1%state.distBetWaves - dist2%state.distBetWaves);
+			var phi = diff*Math.PI/state.distBetWaves;
+			var cosine = Math.abs(Math.cos(phi));
+			
+			/*c.beginPath();
+			c.moveTo(40, i);
+			c.lineTo(45, i);*/
+			var color = Math.floor(cosine*255);
+			var strokeStyle = 'rgb(' + color + ',0,0)';
+			state.colorTable[i] = strokeStyle;
+			state.xWaveTable[i] = 5*cosine;
+			/*c.stroke();*/
+		}
+	}
+	
+	
+	
+	
+	return state;
+}
+
+simulation.render2d = function(state, c, w, h) {
+	c.strokeStyle='rgba(255,0,0,0.5)';
+	for(var i = 0; i <= state.num; i++) {
+		if(state.xPos[i] <= 0) {
+			c.beginPath();
+			c.moveTo(state.xPos[i], -h/2);
+			c.lineTo(state.xPos[i], h/2);
+			c.stroke();
+		} else {
+			c.beginPath();
+			c.arc(0, -state.distBetSlits/2, state.xPos[i], -Math.PI/2, Math.PI/2, false);
+			c.stroke();
+			c.beginPath();
+			c.arc(0, state.distBetSlits/2, state.xPos[i], -Math.PI/2, Math.PI/2, false);
+			c.stroke();
+		}
+	}
+	c.fillStyle="#fff";
+	c.fillRect(40, -h/2, w-90, h);
+	c.beginPath();
+	c.moveTo(40, -h/2);
+	c.lineTo(40, h/2);
+	c.strokeStyle="#000";
+	c.stroke();
+	
+	for(var i = -h/2; i <= h/2; i += .3) {
+		c.beginPath();
+		c.moveTo(40, i);
+		c.lineTo(45, i);
+		c.strokeStyle = state.colorTable[i];
+		c.stroke();
+	}
+	c.beginPath();
+	c.moveTo(40,-h/2);
+	for(var i = -h/2; i < h/2; i += .3) {
+		c.lineTo(40+state.xWaveTable[i], i);
+	}
+	c.strokeStyle = "#0f0";
+	c.stroke();
+	
+	/*for(var i = -h/2; i <= h/2; i += .3) {
+		var dist = (Math.sqrt(Math.pow(i-10, 2) + Math.pow(state.xPos[1],2)))%state.distBetWaves;
+		var phi = dist*2*Math.PI/state.distBetWaves;
+		var dist2 = (Math.sqrt(Math.pow(i+10, 2) + Math.pow(state.xPos[1],2)))%state.distBetWaves;
+		var phi2 = dist2*2*Math.PI/state.distBetWaves;
+		c.lineTo(45 + Math.cos(phi) + Math.cos(phi2), i);
+	}*/
+	
+	
+	//c.fillStyle="#fff";
+	//c.fillRect(-.3, -h/2, .6, h);
+
+	
+	c.beginPath();
+	c.moveTo(0, -h/2);
+	c.lineTo(0, -state.distBetSlits/2-.5);
+	c.moveTo(0, -state.distBetSlits/2+.5);
+	c.lineTo(0, state.distBetSlits/2-.5);
+	c.moveTo(0, state.distBetSlits/2+.5);
+	c.lineTo(0, h/2);
+	c.strokeStyle = "#000";
+	c.stroke();
+}
