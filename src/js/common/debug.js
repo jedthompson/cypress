adc70f48c3dfef8f80ff67a624bbd1f107157c13
Scott Lawrence <bytbox@gmail.com>
2011-08-09 11:07:24 -0400

    Require a DBG.enabled=true line to enable debugging

--- src/js/common/debug.js
+++ src/js/common/debug.js
@@ -3,7 +3,9 @@
 // DBG.write(string) to use
 //
 var DBG = {
+	enabled : false,
 	write : function(txt){
+		if (!this.enabled) return;
 		if (!window.dbgwnd) {
 			window.dbgwnd = window.open("","debug","status=0,toolbar=0,location=0,menubar=0,directories=0,resizable=0,scrollbars=1,width=600,height=250");
 			window.dbgwnd.document.write('<html><head></head><body style="background-color:black"><div id="main" style="color:green;font-size:12px;font-family:Courier New;"></div></body></html>');
